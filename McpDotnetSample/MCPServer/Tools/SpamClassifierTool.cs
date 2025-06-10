cc070caf808c20f1dfb81f1d7d7ff07655a3276c
nurselaltin <nursel.altin@net-bt.com.tr>
2025-06-10 08:44:09 +0300

    Added project and SpamClassifierTool.

---
+++ McpDotnetSample/MCPServer/Tools/SpamClassifierTool.cs
@@ -0,0 +1,17 @@
+﻿using ModelContextProtocol.Protocol;
+
+namespace MCPServer.Tools
+{
+  public  class SpamClassifierTool : Tool
+  {
+    public string Name => "spamClassifierToll";
+    public string Description => "Mailin spam olup olmadığını basit kurallarla kontrol eder.";
+    public  Task<object?> InvokeAsync(object? input)
+    {
+       var mail = input?.ToString()?.ToLowerInvariant() ?? "";
+       var spamWords = new List<string>() { "free", "win", "money","offer", "click"};
+      var isSpam = spamWords.Any(word => mail.Contains(word));
+      return Task.FromResult<object?>(new { isSpam });
+    }
+  }
+}
