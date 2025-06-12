83af610850a6c726137413949dd8d11f853c3dc7
nurselaltin <nursel.altin@net-bt.com.tr>
2025-06-12 07:55:20 +0300

    Edited isSpam method.

--- McpDotnetSample/MCPServer/Tools/SpamClassifierTool.cs
+++ McpDotnetSample/MCPServer/Tools/SpamClassifierTool.cs
@@ -7,12 +7,12 @@ namespace MCPServer.Tools
   public  class SpamClassifierTool
   {
     [McpServerTool, Description("Mailin spam olup olmadığını basit kurallarla kontrol eder.")]
-    public  bool  isSpam(string input)
+    public static Task<bool> isSpam(string input)
     {
-       var mail = input?.ToString()?.ToLowerInvariant() ?? "";
-       var spamWords = new List<string>() { "free", "win", "money","offer", "click"};
+      var mail = input?.ToLowerInvariant() ?? "";
+      var spamWords = new[] { "free", "win", "money", "offer", "click" };
       var isSpam = spamWords.Any(word => mail.Contains(word));
-      return isSpam;
+      return Task.FromResult(isSpam);
     }
   }
 }
