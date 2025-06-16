d236a06200a7bba8cd82df6378daf7b60affbd59
nurselaltin <nursel.altin@net-bt.com.tr>
2025-06-16 09:08:31 +0300

    Add MCP Client project.

---
+++ McpDotnetSample/MCPClient/Program.cs
@@ -0,0 +1,22 @@
+﻿// Client nesnesi oluştur
+using ModelContextProtocol.Client;
+
+//Client connect to Server adresini belirle
+ var clientTransport = new StdioClientTransport(new StdioClientTransportOptions
+ {
+   Name = "MCP Server",
+   Command = @"C:\Users\nursel.altin\source\repos\McpDotnetSample\McpDotnetSample\MCPServer\bin\Debug\net7.0\MCPServer.exe"
+ });
+ 
+//Client Nesnesi oluştur
+var client =  await McpClientFactory.CreateAsync(clientTransport);
+
+//Server tools sınıflarını çek
+var tools = await client.ListToolsAsync();
+
+foreach (var item in tools)
+{
+  Console.WriteLine(item);  
+}
+
+
