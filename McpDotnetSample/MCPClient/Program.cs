cdb7135e393f3e6cda15f43aeee9028d7fa80b68
nurselaltin <nursel.altin@net-bt.com.tr>
2025-06-25 07:43:38 +0300

    FileReaderTool eklendi.

--- McpDotnetSample/MCPClient/Program.cs
+++ McpDotnetSample/MCPClient/Program.cs
@@ -1,22 +1,19 @@
-﻿// Client nesnesi oluştur
-using ModelContextProtocol.Client;
+﻿using ModelContextProtocol.Client;
 
-//Client connect to Server adresini belirle
- var clientTransport = new StdioClientTransport(new StdioClientTransportOptions
- {
-   Name = "MCP Server",
-   Command = @"C:\Users\nursel.altin\source\repos\McpDotnetSample\McpDotnetSample\MCPServer\bin\Debug\net7.0\MCPServer.exe"
- });
- 
-//Client Nesnesi oluştur
-var client =  await McpClientFactory.CreateAsync(clientTransport);
+// MCP Server ile bağlantı kurmak için transport tanımlıyoruz
+var clientTransport = new StdioClientTransport(new StdioClientTransportOptions
+{
+  Name = "MCP Server",
+  Command = @"C:\Users\nursel.altin\source\repos\McpDotnetSample\McpDotnetSample\MCPServer\bin\Debug\net7.0\MCPServer.exe"
+});
 
-//Server tools sınıflarını çek
+// MCP Client nesnesini oluşturuyoruz ve server'a bağlanıyoruz
+var client = await McpClientFactory.CreateAsync(clientTransport);
+
+// Server'da tanımlı tüm tool'ları listeliyoruz
 var tools = await client.ListToolsAsync();
 
-foreach (var item in tools)
+foreach (var tool in tools)
 {
-  Console.WriteLine(item);  
+  Console.WriteLine(tool);
 }
-
-
---
+++ McpDotnetSample/MCPServer/Tools/FileReaderTool.cs
@@ -0,0 +1,25 @@
+﻿using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace MCPServer.Tools
+{
+  [McpServerToolType]
+  public class FileReaderTool
+  {
+    private readonly string[] allowedPaths = new[] { "/app/data/", "/app/logs/" };
+    [McpServerTool, Description("")]
+    public  Task<string> Execute(string path)
+    {
+      // 1. normalize et
+      string normalizedPath = Path.GetFullPath(path);
+
+      // 2. izin verilen path’lerle karşılaştır
+      if (!allowedPaths.Any(allowed => normalizedPath.StartsWith(allowed)))
+        throw new UnauthorizedAccessException("Bu klasöre erişim izni yok.");
+      
+      string path2 = File.ReadAllText(normalizedPath) ?? string.Empty; 
+
+      return Task.FromResult(path2);
+    }
+  }
+}
